dc8e0442e6ce4802d851516405d4bef43a656269
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-04-23 13:57:35 +0900

    修正中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 	def index
   	 @book = Book.new
      @books = Book.all
+     
   end
 
   def show
@@ -16,6 +17,9 @@ class BooksController < ApplicationController
 
   def create
      @book = Book.new(book_params)
+
+     @book.user_id = current_user.id
+
      if @book.save
      	flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)    
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
 
-  root 'post_images#index'
+  root 'tops#top'
   resources :books
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

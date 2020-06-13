e74927173cd2d9dd2d63a019cc1bc1e9773d7ec7
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-06-13 13:22:30 +0900

    deviseモデル修正

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,8 +2,8 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   require 'devise/orm/active_record'  
 
-  ・・・
+  
   config.scoped_views = true
-  ・・・
+  
 
 end
